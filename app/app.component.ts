1d51f81aff88b2a8e7cd818391edda0da886185b
FrankLanglois <franklanglois@gmail.com>
2016-09-19 10:38:52 -0400

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by flangloi on 2016-09-16.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+
+export class AppComponent { }
---
+++ app/app.module.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by flangloi on 2016-09-16.
+ */
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/plaform-browser';
+import {AppComponent} from './app.component'
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
